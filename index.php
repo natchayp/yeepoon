f1c50056a699b52b2c548c8a0afdf3277e274af6
natchayp <73693791+natchayp@users.noreply.github.com>
2020-10-30 17:05:23 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Natcha</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+<body>
+  <h1>Hello User!!!</h1>
+  <p>My name is Yeepoon :)</p>
+  <img src="cat.jpg">
+</body>
+
+</html>
